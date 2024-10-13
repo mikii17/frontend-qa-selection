36b3875e9baa17f0bcb809e6a6224d8ec17f4916
mikii17 <michael12gashaw@gmail.com>
2024-10-13 23:31:52 +0300

    refactor: Update storybook configuration and component paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,8 @@ import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/__stories__/*.mdx",
+    "../components/**/__stories__/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
