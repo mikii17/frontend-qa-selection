6673c07262bbd493090b547205f7fd097f8aef9c
mikii17 <michael12gashaw@gmail.com>
2024-10-13 23:23:24 +0300

    chore: add cypress, storybook and chromatic configuration

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+import { installPlugin } from "@chromatic-com/cypress";
+
+export default defineConfig({
+  e2e: {
+    experimentalStudio: true,
+    setupNodeEvents(on, config) {
+      installPlugin(on, config);
+      // implement node event listeners here
+    },
+  },
+});
